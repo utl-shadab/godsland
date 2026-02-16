f77f9852b9127ba8944f22b2529416aa3d5e2ce9
ami-inn <amipk2001@gmail.com>
2026-02-16 14:53:41 +0530

    update: clean the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './layout/Header';
-import Footer from './layout/Footer';
-import Home from './pages/Home';
-import CreateNFT from './pages/CreateNFT';
-import Explore from './pages/Explore';
-import CollectionDetails from './pages/CollectionDetails';
-import ComingSoon from './pages/ComingSoon';
-import Preloader from './components/Preloader';
-import { LoadingProvider } from './context/LoadingContext';
+import "./index.css";
+
+import IndexRoutes from "./routes/IndexRoutes";
 
 function App() {
-  return (
-    <LoadingProvider>
-      <Router>
-        <Preloader />
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/market" element={<Explore />} />
-          <Route path="/market/:category" element={<Explore />} />
-          <Route path="/market/:category/:collectionId" element={<CollectionDetails />} />
-          <Route path="/create" element={<CreateNFT />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/collections/:slug" element={<CollectionDetails />} />
-          <Route path="/auction" element={<ComingSoon />} />
-          <Route path="/club" element={<ComingSoon />} />
-          <Route path="/collections" element={<ComingSoon />} />
-          <Route path="/pages" element={<ComingSoon />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </LoadingProvider>
-  );
+  return <IndexRoutes />;
 }
 
 export default App;
