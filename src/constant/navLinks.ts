9c74d97a88e4d46050b54d5b64d8fcd1ccd4aac3
uti-shadab <shadab.dev.utl@gmail.com>
2026-02-22 12:53:34 +0530

    merged with ami

--- src/constant/navLinks.ts
+++ src/constant/navLinks.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
     { to: "/user/owned/", label: "My Collections" },
-    { to: "/buy-sell", label: "Buy/Sell" },
+    { to: "/market", label: "Buy/Sell" },
     { to: "/user/profile", label: "Profile" },
     { to: "/user/settings", label: "Manage Account" },
 ];
